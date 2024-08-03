8f88ae74d43f6fe86b1a02c4a8ac7d1fc6de118a
nicvaltel <nicvaltel@gmail.com>
2024-08-03 20:18:15 +0300

    Send workspace info from xmonad to xmobar via logHook dynamicLogWithPP

--- dereference/.xmonad/xmonad.hs
+++ dereference/.xmonad/xmonad.hs
@@ -3,7 +3,7 @@ import XMonad.Actions.SpawnOn(spawnOn, spawnAndDo, manageSpawn)
 import XMonad.Config.Desktop (desktopConfig)
 import XMonad.Hooks.ManageDocks (avoidStruts)
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.DynamicLog (PP(..), wrap, shorten, xmobarColor)
+import XMonad.Hooks.DynamicLog (PP(..), wrap, shorten, xmobarColor, dynamicLogWithPP)
 import XMonad.Layout.NoBorders (noBorders)
 import XMonad.Layout.Tabbed (Theme (..), shrinkText, tabbed)
 import XMonad.Layout.Spacing(spacing, spacingRaw, Border(..))
@@ -13,7 +13,7 @@ import XMonad.ManageHook(doShift)
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.SessionStart(doOnce)
 import XMonad.Util.SpawnOnce (spawnOnce, spawnOnOnce)
-import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.Run (spawnPipe, hPutStrLn)
 import qualified Data.Map as M
 import qualified XMonad.Layout.Dwindle as Dwindle
 import qualified XMonad.Hooks.DynamicLog as DL
@@ -21,7 +21,8 @@ import qualified XMonad.Hooks.DynamicLog as DL
 
 -- TODO : The ewmhFullscreen function does not exist in these versions. Instead of it, you can try to add fullscreenEventHook to your handleEventHook to achieve similar functionality (how to do this is explained in the documentation of XMonad.Hooks.EwmhDesktops
 main = do
-  xmproc <- spawnPipe "xmobar -x 0 ~/.config/xmobar/xmobar.config"
+  -- xmproc <- spawnPipe "xmobar -x 0 ~/.config/xmobar/xmobar.config"
+  xmproc <- spawnPipe "xmobar ~/.config/xmobar/xmobar.config"
   -- xmonad . ewmh . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey $
   xmonad $ 
     desktopConfig
@@ -33,10 +34,12 @@ main = do
         layoutHook = myLayout,
         startupHook = myStartupHook,
         workspaces = myWorkspaces,
-        manageHook = manageSpawn <+> manageHook def -- manageSpawn need for spawnOn/spawnOnOnce 
+        manageHook = manageSpawn <+> manageHook def, -- manageSpawn need for spawnOn/spawnOnOnce 
+        logHook =  myLogHook xmproc
       } `additionalKeys` (myAdditionalKeys modm)
   where
     modm = mod4Mask
+    
 
 myAdditionalKeys modm =
   [ ((modm, xK_slash), spawn "feh --zoom 180 ~/my/wallpapers/xmbindings.png")
@@ -87,14 +90,14 @@ myTabConfig =
       fontName = "xft:Noto Sans CJK:size=10:antialias=true"
     }
 
-myWorkspaces = ["ws1","ws2","ws3","ws4","ws5","ws6","ws7","ws8","ws9"]
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
 
 myStartupHook :: X ()
 myStartupHook = do
   -- spawnOnce "feh --bg-scale --no-fehbg /home/kolay/my/wallpapers/xmbindings.png"
   -- spawnOnce "feh --bg-center --no-fehbg ~/my/wallpapers/composition-a-xxi-1925.jpg"
   spawnOnce "feh --bg-max --no-fehbg ~/my/wallpapers/geysha.png"
-  spawnOnOnce "ws9" "x-terminal-emulator -e ~/hacky.sh ~/.xmonad/vpn.sh"
+  spawnOnOnce "9" "x-terminal-emulator -e ~/hacky.sh ~/.xmonad/vpn.sh"
 
 
 myXmobarPP :: PP
@@ -124,3 +127,46 @@ myXmobarPP = def
     yellow   = xmobarColor "#f1fa8c" ""
     red      = xmobarColor "#ff5555" ""
     lowWhite = xmobarColor "#bbbbbb" ""
+
+
+myLogHook proc = dynamicLogWithPP $ DL.xmobarPP
+  { ppOutput  = hPutStrLn proc
+  , ppCurrent = currentStyle
+  , ppVisible = visibleStyle
+  , ppTitle   = titleStyle
+  , ppHiddenNoWindows = \wsId -> if any (`elem` wsId) (['a'..'z']) then wsId else "_"
+  , ppLayout = const "" 
+      -- \layout -> case layout of
+      --   "Tall"        -> "[|]"
+      --   "Mirror Tall" -> "[-]"
+      --   "ThreeCol"    -> "[||]"
+      --   "Tabbed"      -> "[_]"
+      --   "Gimp"        -> "[&]"
+      --   x             -> x
+  }
+  where
+    currentStyle = xmobarColor "yellow" "" . wrap "[" "]"
+    visibleStyle = wrap "(" ")"
+    titleStyle   = xmobarColor "cyan" "" . shorten 100 . filterCurly
+    filterCurly  = filter (not . isCurly)
+    isCurly x    = x == '{' || x == '}'
+
+myLogHook2 proc = dynamicLogWithPP $ DL.defaultPP {
+            ppOutput = hPutStrLn proc
+          , ppTitle = xmobarColor "#119999" "" . shorten 12
+          , ppCurrent = xmobarColor "#229999" "" . wrap "[" "]"
+          , ppSep = "   "
+          , ppWsSep = " "
+          , ppLayout  = \x -> ""
+              --   (\ x -> case x of
+              -- "Spacing 6 Mosaic"                      -> "[:]"
+              -- "Spacing 6 Mirror Tall"                 -> "[M]"
+              -- "Spacing 6 Hinted Tabbed Simplest"      -> "[T]"
+              -- "Spacing 6 Full"                        -> "[ ]"
+              -- _                                       -> x )
+          , ppHiddenNoWindows = showNamedWorkspaces
+          }
+  where
+    showNamedWorkspaces wsId = if any (`elem` wsId) (['a'..'z'])
+      then wsId
+      else "_"
